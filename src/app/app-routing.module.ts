516119e51b8950112a2fd8a36c39613b7867c294
ivvan18 <iatokarev@edu.hse.ru>
2018-05-24 14:48:08 +0300

    Layout completely done.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Section1Component} from './section1/section1.component';
 import {Section2Component} from './section2/section2.component';
+import {Section3Component} from './section3/section3.component';
 
 const routes: Routes = [
   { path: 'section1', component: Section1Component },
   { path: 'section2', component: Section2Component },
+  { path: 'section3', component: Section3Component },
   { path: '', redirectTo: '/section1', pathMatch: 'full' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { Section1Component } from './section1/section1.component';
 import { Section2Component } from './section2/section2.component';
+import { Section3Component } from './section3/section3.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     Section1Component,
-    Section2Component
+    Section2Component,
+    Section3Component
   ],
   imports: [
     BrowserModule,
